b1acc78c5a4df4f345d0854abf4a7e593dacb2c3
Mario Aburto <marioaburto10@gmail.com>
2018-10-31 00:18:51 -0400

    added a form and /send route

--- app.py
+++ app.py
@@ -15,5 +15,15 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+# Route that renders the form and receives form data
+# Query the database and send the jsonified results if route hit via a post method
+@app.route("/send", methods=["GET", "POST"])
+def send():
+    if request.method == "POST":
+        print(request.form)
+        return redirect("/", code=302)
+
+    return render_template("form.html")
+
 if __name__ == "__main__":
     app.run()
